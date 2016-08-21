96a43845ceb46f1ab68b3d82a04ec6b6cca23269
SonicFreak94 <sonicfreak94@hotmail.com>
2016-08-21 13:37:38 -0600

    Add project files.

---
+++ sadx-model-view/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sadx_model_view
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
