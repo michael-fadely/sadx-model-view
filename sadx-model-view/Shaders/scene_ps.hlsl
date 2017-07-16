d035408a57b661ce00166a3564cd94ab4a099d58
Michael Fadely <michaelwfadely@gmail.com>
2017-07-16 16:17:08 -0600

    only discard pixels when alpha blending is enabled

--- sadx-model-view/Shaders/scene_ps.hlsl
+++ sadx-model-view/Shaders/scene_ps.hlsl
@@ -24,7 +24,7 @@ float4 main(VS_OUTPUT input) : SV_TARGET
 
 	result = result * input.diffuse + input.specular;
 
-	if (result.a < 16.0f / 255.0f)
+	if (material.useAlpha && result.a < 16.0f / 255.0f)
 	{
 		discard;
 	}
